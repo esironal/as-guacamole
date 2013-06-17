b79f0bda324e0e7e0527291026bda6e580d05d2f
portrain <port.rain@gmail.com>
2013-06-17 17:54:00 +1000

    Split call of vncserver

--- src/main/java/net/sourceforge/guacamole/net/ausyncguac/AusyncGuacamoleTunnelServlet.java
+++ src/main/java/net/sourceforge/guacamole/net/ausyncguac/AusyncGuacamoleTunnelServlet.java
@@ -34,7 +34,8 @@ public class AusyncGuacamoleTunnelServlet extends GuacamoleHTTPTunnelServlet {
         try {
             //Use ProcessBuilder to allow for arguments with spaces (such as file paths)
             List<String> command = new ArrayList<String>();
-            command.add("sudo -u "+username);
+            command.add("/usr/bin/sudo");
+            command.add("-u "+username);
             command.add("/usr/bin/vncserver");
             for (String s : args) {
                 command.add(s);
